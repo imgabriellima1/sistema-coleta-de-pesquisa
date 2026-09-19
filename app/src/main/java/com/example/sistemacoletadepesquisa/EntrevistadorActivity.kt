e9cb46697cfe72924ecde51404371df21750d137
Gabriel Lima <gabrielfreitasdelima@gmail.com>
2026-09-19 16:22:54 -0300

    telas dos tipos de pesquisa criadas

--- app/src/main/java/com/example/sistemacoletadepesquisa/EntrevistadorActivity.kt
+++ app/src/main/java/com/example/sistemacoletadepesquisa/EntrevistadorActivity.kt
@@ -1,6 +1,8 @@
 package com.example.sistemacoletadepesquisa
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class EntrevistadorActivity : AppCompatActivity() {
@@ -9,5 +11,57 @@ class EntrevistadorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_entrevistador)
+
+        val btnPesquisaCompleta =
+            findViewById<Button>(R.id.btnPesquisaCompleta)
+
+        val btnEspontanea =
+            findViewById<Button>(R.id.btnEspontanea)
+
+        val btnEstimulada =
+            findViewById<Button>(R.id.btnEstimulada)
+
+        val btnProblemas =
+            findViewById<Button>(R.id.btnProblemas)
+
+        btnPesquisaCompleta.setOnClickListener {
+
+            val intent = Intent(
+                this,
+                EspontaneaActivity::class.java
+            )
+
+            startActivity(intent)
+        }
+
+        btnEspontanea.setOnClickListener {
+
+            val intent = Intent(
+                this,
+                EspontaneaActivity::class.java
+            )
+
+            startActivity(intent)
+        }
+
+        btnEstimulada.setOnClickListener {
+
+            val intent = Intent(
+                this,
+                EstimuladaActivity::class.java
+            )
+
+            startActivity(intent)
+        }
+
+        btnProblemas.setOnClickListener {
+
+            val intent = Intent(
+                this,
+                ProblemasActivity::class.java
+            )
+
+            startActivity(intent)
+        }
     }
 }
